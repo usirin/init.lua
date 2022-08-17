90faceb9049640198dc8dda73821765975b7815a
ussirin <ussirin@twitch.tv>
2022-08-16 21:43:31 -0700

    Update colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,6 @@
+-- vim.cmd([[set bg=light]])
+vim.cmd([[set bg=dark]])
 vim.cmd([[colorscheme bleed-purple]])
 
 vim.cmd([[hi Search ctermbg=NONE ctermfg=NONE cterm=underline]])
 vim.cmd([[hi Search guibg=NONE guifg=NONE gui=underline]])
-
