47485cda8b398f7f8c24556dfb324fd417b13cc0
ussirin <ussirin@twitch.tv>
2022-08-16 21:42:17 -0700

    Configure lightline for light bg mode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -20,7 +20,7 @@ local filename = function()
   return path .. " " .. status
 end
 
-local theme = {
+local dark_theme = {
   normal = {
     a = {bg = colors.bmagenta, fg = colors.bblack, gui = "bold"},
     b = {bg = colors.bgreen, fg = colors.black},
@@ -37,11 +37,35 @@ local theme = {
   }
 }
 
+local light_theme = {
+  normal = {
+    a = {bg = colors.bmagenta, fg = colors.bblack, gui = "bold"},
+    b = {bg = colors.bgreen, fg = colors.black},
+    c = {bg = colors.white, fg = colors.black}
+  },
+  insert = {a = {bg = colors.green, fg = colors.black, gui = "bold"}},
+  visual = {a = {bg = colors.yellow, fg = colors.black, gui = "bold"}},
+  replace = {a = {bg = colors.red, fg = colors.black, gui = "bold"}},
+  command = {a = {bg = colors.bblue, fg = colors.black, gui = "bold"}},
+  inactive = {
+    a = {bg = colors.white, fg = colors.dimmed_text},
+    b = {bg = colors.white, fg = colors.dimmed_text},
+    c = {bg = colors.white, fg = colors.dimmed_text}
+  }
+}
+
+local get_theme = function(bg)
+  if bg == "light" then
+    return light_theme
+  end
+  return dark_theme
+end
+
 require"lualine".setup {
   options = {
     icons_enabled = true,
     -- theme = "horizon",
-    theme = theme,
+    theme = get_theme(vim.o.background),
     -- theme = "auto",
     component_separators = {left = "", right = ""},
     section_separators = {left = "", right = ""},
