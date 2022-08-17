bba93cbc938fbbb0aa5de0aaeb43fd76fdc91e0a
ussirin <ussirin@twitch.tv>
2022-08-16 21:41:39 -0700

    Customize startify

--- lua/plugins/startify.lua
+++ lua/plugins/startify.lua
@@ -1,1 +1,10 @@
 vim.g.startify_session_dir = '~/nvim-sessions'
+vim.g.startify_bookmarks = {"~/dotfiles", "~/.config"}
+
+vim.g.startify_lists = {
+  { type = 'dir',       header = {'   MRU ' .. vim.fn.getcwd()} },
+  { type = 'files',     header = {'   MRU'}            },
+  { type = 'sessions',  header = {'   Sessions'}       },
+  { type = 'bookmarks', header = {'   Bookmarks'}      },
+  { type = 'commands',  header = {'   Commands'}       },
+}
